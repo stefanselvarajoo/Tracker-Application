0b9fea309aa8585e63a7ec79c6a12d89b796425f
NightHawk-Michael <michael.le611@gmail.com>
2017-02-21 02:35:29 +0800

    Initialize App

---
+++ TrackerApplication/app/src/main/java/com/michael/trackerapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.michael.trackerapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
